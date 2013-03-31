d2d4ecf1ebba0609e1f1a7787bce077c7cceec5c
Scott Bartram <scottb@bwcgroup.com>
2013-03-31 12:30:46 -0400

    finished and submitted

--- recfun/src/main/scala/recfun/Main.scala
+++ recfun/src/main/scala/recfun/Main.scala
@@ -1,32 +1,25 @@
 package recfun
-import common._
-import scala.collection.mutable.Stack
 
 object Main {
 
-    //    def main(args: Array[String]) {
-    //        println("Pascal's Triangle")
-    //        for (row <- 0 to 10) {
-    //            for (col <- 0 to row)
-    //                print(pascal(col, row) + " ")
-    //            println()
-    //        }
-    //    }
+    def main(args: Array[String]) {
+        println("Pascal's Triangle")
+        for (row <- 0 to 10) {
+            for (col <- 0 to row)
+                print(pascal(col, row) + " ")
+            println()
+        }
+    }
 
     /**
      * Exercise 1
      */
-    def pascal(c: Int, r: Int): Int = { 0 }
-
-    def main(args: Array[String]) {
-        println("result(T): " + balance("".toList))
-        println("result(F): " + balance(")".toList))
-        println("result(T): " + balance("()".toList))
-        println("result(F): " + balance("())".toList))
-        println("result(F): " + balance("(()(".toList))
-        println("result(T): " + balance("()()".toList))
-        println("result(T): " + balance("(()())".toList))
-        println("result(F): " + balance("())(".toList))
+    def pascal(c: Int, r: Int): Int = {
+        c match {
+            case 0 => 1
+            case i if i > r => 0
+            case _ => pascal(c - 1, r - 1) + pascal(c, r - 1)
+        }
     }
 
     /**
@@ -34,18 +27,18 @@ object Main {
      */
     def balance(chars: List[Char]): Boolean = {
 
-        def m(cs: List[Char], stack: Int): Boolean = {
-            if (stack < 0)
+        def balIter(cs: List[Char], stackCount: Int): Boolean = {
+            if (stackCount < 0)
                 false
             else if (cs.isEmpty)
-                stack == 0
+                stackCount == 0
             else if (cs.head == '(')
-                m(cs.tail, stack + 1)
+                balIter(cs.tail, stackCount + 1)
             else // ')'
-                m(cs.tail, stack - 1)
+                balIter(cs.tail, stackCount - 1)
         }
 
-        m(chars.filter(c => c == '(' || c == ')'), 0)
+        balIter(chars.filter(c => c == '(' || c == ')'), 0)
     }
 
     /**
@@ -53,13 +46,13 @@ object Main {
      */
     def countChange(money: Int, coins: List[Int]): Int = {
 
-        def c(m: Int, cs: List[Int]): Int = {
+        def countIter(m: Int, cs: List[Int]): Int = {
             m match {
                 case 0 => 1
-                case i if i > 0 && !cs.isEmpty => c(i - cs.head, cs) + c(i, cs.tail)
+                case i if i > 0 && !cs.isEmpty => countIter(i - cs.head, cs) + countIter(i, cs.tail)
                 case _ => 0
             }
         }
-        c(money, coins.sortWith((a, b) => a > b))
+        countIter(money, coins.sortWith((a, b) => a > b))
     }
 }
